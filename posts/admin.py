a95e96685efd821461ce68033d7a56c19e290355
Janell Huyck <Janell.Huyck@outlook.com>
2023-05-19 23:40:25 -0400

    Created blog models, migration

--- posts/admin.py
+++ posts/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
 
+from posts.models import Author, Post, Commenter, Comment, Category, Tag
+
 # Register your models here.
+admin.site.register(Author)
+admin.site.register(Post)
+admin.site.register(Commenter)
+admin.site.register(Comment)
+admin.site.register(Category)
+admin.site.register(Tag)
+
+
--- posts/models.py
+++ posts/models.py
@@ -1,58 +1,191 @@
 from django.db import models
+from django.utils import timezone
+# from django.contrib.auth.models import User
+# from django.urls import reverse
+# from django.utils.text import slugify
+# from django.utils.html import mark_safe
+# from django.utils.safestring import SafeString
+# from django.utils.safestring import mark_safe
+# from markdown import markdown
 
-# Create your models here.
-"""
-Author
-    - Name - dictionary {First Name: string(50), Last Name:string(50)}
-    - Email - {Email: string(50)}
-    - Bio - string(5000)
-    - Profile Picture - image
-    - Slug - string(100)
-    - Status - [Active, Inactive]
-    - Posts - list of posts
-   
-Post
-    - Title - string(100)
-    - Content - text
-    - Author - foreign key to Author
-    - Date - datetime
-    - Last Updated Date - datetime
-    - Image1 - Image
-    - Image2 - Image
-    - Image3 - Image
-    - Image4 - Image
-    - Category - foreign key to Category
-    - Tags - list of tags
-    - Slug - string(100)
-    - Status - [Draft, Published, Deleted]
-    - Featured - boolean
-    - Views - integer
-    - Likes - integer
-    - Comments - list of comments
-    - Shares - integer
+class Author(models.Model):
+    first_name = models.CharField(max_length=50)
+    last_name = models.CharField(max_length=50)
+    email = models.EmailField()
+    bio = models.CharField(max_length=5000)
+    profile_picture = models.ImageField(upload_to='profile_pictures')
+    slug = models.SlugField(max_length=100)
+    status = models.CharField(max_length=10)
 
-Commenter
-    - Name - string(50)
-    - Email - string(50)
+    def __str__(self):
+        return f"{self.first_name} {self.last_name}"
+    
+    def get_absolute_url(self):
+        return reverse('author-detail', kwargs={'slug': self.slug})
+    
+    def get_posts(self):
+        return Post.objects.filter(author=self)
+    
+    def get_posts_count(self):
+        return Post.objects.filter(author=self).count()
+    
+    def get_full_name(self):
+        return f"{self.first_name} {self.last_name}"
+    
+    def get_short_name(self):
+        return self.first_name
+    
+    def get_bio_as_markdown(self):
+        return mark_safe(markdown(self.bio, safe_mode='escape'))
+    
+    def get_status(self):
+        return self.status
+    
+    def get_profile_picture(self):
+        return self.profile_picture
+    
+    def get_email(self):
+        return self.email
+    
+    def get_slug(self):
+        return self.slug
+    
+class Post(models.Model):
+    title = models.CharField(max_length=100)
+    content = models.TextField()
+    author = models.ForeignKey(Author, on_delete=models.CASCADE)
+    date = models.DateTimeField(auto_now_add=True)
+    last_updated_date = models.DateTimeField(auto_now=True)
+    image1 = models.ImageField(upload_to='post_images')
+    image2 = models.ImageField(upload_to='post_images')
+    image3 = models.ImageField(upload_to='post_images')
+    image4 = models.ImageField(upload_to='post_images')
+    # category = models.ForeignKey(Category, on_delete=models.CASCADE)
+    # tags = models.ManyToManyField(Tag)
+    slug = models.SlugField(max_length=100)
+    status = models.CharField(max_length=10)
+    featured = models.BooleanField(default=False)
+    views = models.IntegerField(default=0)
+    likes = models.IntegerField(default=0)
+    shares = models.IntegerField(default=0)
+    
+    def __str__(self):
+        return self.title
+    
+    def get_absolute_url(self):
+        return reverse('post-detail', kwargs={'slug': self.slug})
+    
+    def get_author(self):
+        return self.author
+    
+    def get_content_as_markdown(self):
+        return mark_safe(markdown(self.content, safe_mode='escape'))
+    
+    def get_date(self):
+        return self.date
+    
+    def get_last_updated_date(self):
+        return self.last_updated_date
+    
+    def get_image1(self):
+        return self.image1
+    
+    def get_image2(self):
+        return self.image2
+    
+    def get_image3(self):
+        return self.image3
+    
+    def get_image4(self):
+        return self.image4
+    
+    def get_slug(self):
+        return self.slug
+    
+    def get_status(self):
+        return self.status
+    
+    def get_featured(self):
+        return self.featured
+    
+    def get_views(self):
+        return self.views
+    
+    def get_likes(self):
+        return self.likes
+    
+    def get_shares(self):
+        return self.shares
 
-Comment
-    - Commenter - foreign key to Commenter
-    - Comment - string(500)
-    - Date - datetime
-    - Post - foreign key to Post
-    - Status - [Published, Censored, Deleted]
-    - Likes - integer
-    - Replies - list of comments
+class Commenter(models.Model):
+    name = models.CharField(max_length=50)
+    email = models.EmailField()
+    
+    def __str__(self):
+        return self.name
+    
+    def get_name(self):
+        return self.name
+    
+    def get_email(self):
+        return self.email
+    
+class Comment(models.Model):
+    commenter = models.ForeignKey(Commenter, on_delete=models.CASCADE)
+    comment = models.CharField(max_length=500)
+    date = models.DateTimeField(auto_now_add=True)
+    post = models.ForeignKey(Post, on_delete=models.CASCADE)
+    status = models.CharField(max_length=10)
+    likes = models.IntegerField(default=0)
+    
+    def __str__(self):
+        return self.comment
+    
+    def get_commenter(self):
+        return self.commenter
+    
+    def get_comment(self):
+        return self.comment
+    
+    def get_date(self):
+        return self.date
+    
+    def get_post(self):
+        return self.post
+    
+    def get_status(self):
+        return self.status
+    
+    def get_likes(self):
+        return self.likes
+    
+class Category(models.Model):
+    name = models.CharField(max_length=50)
+    description = models.CharField(max_length=500)
+    
+    def __str__(self):
+        return self.name
+    
+    def get_name(self):
+        return self.name
+    
+    def get_description(self):
+        return self.description
+    
+class Tag(models.Model):
+    name = models.CharField(max_length=50)
+    description = models.CharField(max_length=500)
+    
+    def __str__(self):
+        return self.name
+    
+    def get_name(self):
+        return self.name
+    
+    def get_description(self):
+        return self.description
+    
 
-Category
-    - Name - string(50)
-    - Description - string(500)
-    - Posts - list of posts
+# stopping at 33:47 on video, moving over to Hackerrank for interview prep for a bit
 
-Tag
-    - Name - string(50)
-    - Description - string(500)
-    - Posts - list of posts
 
-stopping at 33:47 on video, moving over to Hackerrank for interview prep for a bit
-"""
